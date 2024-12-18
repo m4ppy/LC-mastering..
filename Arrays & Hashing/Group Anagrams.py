8164e3049f20376bb3fb0912d73471a08668b48c
Mappy78 <99581542+Mappy78@users.noreply.github.com>
2024-12-18 23:31:05 +0900

    Update Group Anagrams.py

--- Arrays & Hashing/Group Anagrams.py
+++ Arrays & Hashing/Group Anagrams.py
@@ -1,11 +1,11 @@
 # Approach 1: Hash Map
-# Time complexity: O(n) because we traverse the list once.
-# Space complexity: O(n) for storing elements in the hash map.
+# Time complexity: O(n * m * logm) for sorting each string (n is the number of strings, m is the average length of each string).
+# Space complexity: O(n * m) for storing elements in the hash map (total number of characters across all strings).
 
 class Solution:
   def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    #approach: Hash Map
+    # approach: Hash Map
     str_map = collections.defaultdict(list)
-    for s in strs:
-      str_map["".join(sorted(s))].append(s)
-    return [l for l in str_map.values()]
+    for s in strs:  # O(n)
+      str_map["".join(sorted(s))].append(s)  # O(m * logm) for sorting
+    return [l for l in str_map.values()]  # O(n)
